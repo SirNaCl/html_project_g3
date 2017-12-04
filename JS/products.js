e921152169322a130b442523fe32a95235f42927
Calrowe <bjornadell@hotmail.com>
2017-12-04 14:25:48 +0100

    Fixed infobuttons

--- JS/products.js
+++ JS/products.js
@@ -1,8 +1,48 @@
 $(document).ready(function(){
 
     $("#blackCoffee").on("click",function(){
-        $(this).closest("div").closest("li").find("div").classRemove("hidden");
+        if($(this).closest("div").closest("li").find("div:nth-child(2)").hasClass("hidden")) {
+            $(this).closest("div").closest("li").find("div").removeClass("hidden");
+        }
+        else{
+            $(this).closest("div").closest("li").find("div:nth-child(2)").addClass("hidden");
+        }
     });
 
+    $("#cappuccino").on("click",function(){
+        if($(this).closest("div").closest("li").find("div:nth-child(2)").hasClass("hidden")) {
+            $(this).closest("div").closest("li").find("div").removeClass("hidden");
+        }
+        else{
+            $(this).closest("div").closest("li").find("div:nth-child(2)").addClass("hidden");
+        }
+    });
+
+    $("#espresso").on("click",function(){
+        if($(this).closest("div").closest("li").find("div:nth-child(2)").hasClass("hidden")) {
+            $(this).closest("div").closest("li").find("div").removeClass("hidden");
+        }
+        else{
+            $(this).closest("div").closest("li").find("div:nth-child(2)").addClass("hidden");
+        }
+    });
+
+    $("#irishCoffee").on("click",function(){
+        if($(this).closest("div").closest("li").find("div:nth-child(2)").hasClass("hidden")) {
+            $(this).closest("div").closest("li").find("div").removeClass("hidden");
+        }
+        else{
+            $(this).closest("div").closest("li").find("div:nth-child(2)").addClass("hidden");
+        }
+    });
+
+    $("#greenTea").on("click",function(){
+        if($(this).closest("div").closest("li").find("div:nth-child(2)").hasClass("hidden")) {
+            $(this).closest("div").closest("li").find("div").removeClass("hidden");
+        }
+        else{
+            $(this).closest("div").closest("li").find("div:nth-child(2)").addClass("hidden");
+        }
+    });
 });
 
